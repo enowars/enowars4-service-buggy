c5ae3118373923a700f49a9d3f0a4676a50aadcd
Henrik <henrik.nickel@campus.tu-berlin.de>
2020-05-16 15:20:17 +0200

    added simple mail function

--- service/mailconnector.go
+++ service/mailconnector.go
@@ -13,6 +13,16 @@ import (
 	gomail "gopkg.in/mail.v2"
 )
 
-func main() {
+func sendMail() {
+	m := gomail.NewMessage()
+	m.SetHeader("From", "foobar@apple.com")
+	m.SetHeader("To", "bbla")
+	m.SetAddressHeader("Cc", "dan@example.com", "Dan")
+	m.SetHeader("Subject", "Hello")
+	m.SetBody("text", "Hello")
+	d := gomail.Dialer{Host: "127.0.0.1", Port: 587}
 
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
 }
